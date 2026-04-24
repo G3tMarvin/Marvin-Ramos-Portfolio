bee341804d4736dcdb5cebc98aeaa84497ad586c
G3tMarvin <marviniramos200@gmail.com>
2026-04-24 01:44:42 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  revealElements.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
